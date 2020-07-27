686d40b44260b977c18fb2d71bc168b754157882
David Colatti <davidcolatti@gmail.com>
2020-07-27 11:57:50 -0400

    handlebars app

--- app.js
+++ app.js
@@ -2,10 +2,55 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const handlebars = require("express-handlebars");
+
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: "hbs",
+    defaultLayout: "index",
+    partialsDir: `${__dirname}/views/partials`,
+  })
+);
+
 app.use(express.static("public"));
 
+const fakeApi = () => {
+  return [
+    {
+      name: "Katarina",
+      lane: "midlaner",
+    },
+    {
+      name: "Jayce",
+      lane: "toplaner",
+    },
+    {
+      name: "Heimerdinger",
+      lane: "toplaner",
+    },
+    {
+      name: "Zed",
+      lane: "midlaner",
+    },
+    {
+      name: "Azir",
+      lane: "midlaner",
+    },
+  ];
+};
+
+const list = true;
+
 app.get("/", (req, res) => {
-  res.send("index");
+  res.render("main", {
+    layout: "index",
+    suggestedChamps: fakeApi(),
+    listExists: list,
+  });
 });
 
 app.listen(PORT, () => {
